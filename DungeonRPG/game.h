10d7dc00832f56194c13f791f18ae98a673cd27d
Nacht <nassen.nacht@gmail.com>
2026-04-20 00:14:32 +0300

    Update game.h

--- DungeonRPG/game.h
+++ DungeonRPG/game.h
@@ -18,6 +18,51 @@ public:
         enemies[1] = new Enemy("Orc", "orc", 80, 20, 60, 35);
     }
 
+    void shop() {
+        bool onShop = true;
+
+        do {
+            std::cout << "\n+========= SHOP =========+"
+                      << "\n1. Heal Potion       20 Gold"
+                      << "\n2. Sword \"Zirael\"  35 Gold"
+                      << "\n0. Exit\n";
+
+            std::cout << "Select: ";
+            int select = 0;
+            std::cin >> select;
+
+            switch (select) {
+                case 1:
+                    if (player->getGold() >= 20) {
+                        player->getInventory()->addItem(new Item("Heal Potion", "Potion", 0, 50));
+                        player->addGold(-20);
+                    }
+                    else {
+                        std::cout << "Not enough gold!\n";
+                        break;
+                    }
+                    break;
+                case 2:
+                    if (player->getGold() >= 35) {
+                        player->getInventory()->addItem(new Item("Zirael", "Sword", 25, 0));
+                        player->addGold(-35);
+                    }
+                    else {
+                        std::cout << "Not enough gold!\n";
+                        break;
+                    }
+                    break;
+                case 0:
+                    std::cout << "\nExiting the shop\n";
+                    onShop = false;
+                    break;
+                default:
+                    std::cout << "\nThe item does not exist\n";
+                    break;
+            }
+        } while (onShop);
+    }
+
     void playerStatus() {
         std::cout << "Name:  " << player->getName()
                   << "\nDMG:   " << player->attack()
@@ -73,9 +118,11 @@ public:
         }
         else {
             std::cout << enemy->getName() << " defeated! +" << enemy->getGold() << " gold!\n";
-            player->addGold(enemy->getGold());
+
+            player->addGold(enemy->getGold()); // Add gold to player after enemy defeated
             player->addExperience(enemy->getGiveExp());
             player->levelUp();
+
             if (rand() % 2 == 0) {
                 player->getInventory()->addItem(new Item("Heal Potion", "potion", 0, 50));
                 std::cout << "You found a Heal Potion!\n";
@@ -90,9 +137,10 @@ public:
     }
 
     void mainMenu() {
-        std::cout << "\n==== Menu ====\n"
+        std::cout << "\n+==== Menu ====+\n"
                   << "1 - Research\n"
                   << "2 - Player status\n"
+                  << "3 - Shop\n"
                   << "0 - Exit\n";
         int select = 0;
         std::cin >> select;
@@ -103,6 +151,9 @@ public:
                 break;
             case 2:
                 playerStatus();
+                break;
+            case 3:
+                shop();
                 break;
             case 0:
                 running = false;
